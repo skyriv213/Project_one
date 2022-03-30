8e1031cd612a9742812b247dd01778de1a2886de
skyriv <gn1007@dankook.ac.kr>
2022-03-30 21:28:58 +0900

    todolist 0.1 기본설

---
+++ ToDoList/src/main/java/com/example/ToDoList/ApiResponse/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.example.ToDoList.ApiResponse;
+
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter @Setter
+@RequiredArgsConstructor
+public abstract class ApiResponse<T> {
+    @NonNull private T data;
+    private List<String> errors;
+}
+
+
---
+++ ToDoList/src/main/java/com/example/ToDoList/ToDoItem/ToDoItemAdapter.java
@@ -0,0 +1,12 @@
+package com.example.ToDoList.ToDoItem;
+
+import java.util.List;
+
+public class ToDoItemAdapter {
+
+    public static ToDoItemResponse toToDoItemResponse(final ToDoItem toDoItem, final List<String> errors) {
+        return ToDoItemResponse.builder().toDoItem(toDoItem).errors(errors).build();
+    }
+}
+
+
