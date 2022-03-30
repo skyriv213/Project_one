1d8c8b24330af9412fc1e34a8946bc5013b73947
skyriv <gn1007@dankook.ac.kr>
2022-03-30 14:34:06 +0900

    주석 입력

--- todolist/src/main/java/com/example/todolist/ToDoItem/ToDoItemService.java
+++ todolist/src/main/java/com/example/todolist/ToDoItem/ToDoItemService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service // Service 어노테이션은 id를 받아 JPA Repository에 넣어줄 예정
 public class ToDoItemService{
-
+    // 데이터베이스가 없으니 임의의 값을 리턴
     public ToDoItem get(final String id){
         return ToDoItem.builder()
                 .title("Add an id Validation")
